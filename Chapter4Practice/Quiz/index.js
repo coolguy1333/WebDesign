b746f971d6bac66e2243ffcdca3e232185c9e40e
coolguy1333 <finglaws@stu.masd.k12.wi.us>
2026-03-12 13:04:52 -0500

    plug

--- Chapter4Practice/Quiz/index.js
+++ Chapter4Practice/Quiz/index.js
@@ -1,6 +1,10 @@
 document.getElementById("carPartQuiz").addEventListener("submit",
     (event) => {
         event.preventDefault();
-        console.log("event listener success!")
+        let rearAxle = 0;
+        let frontRightFender = 0;
+        let rim = 0;
+        let exhaustPipe = 0;
+        let sparkPlug = 0;
     }
 )
