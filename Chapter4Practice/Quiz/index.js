15a42ba8cd3a29a539280e9bdced56e6eeeac1e2
coolguy1333 <finglaws@stu.masd.k12.wi.us>
2026-03-13 14:32:35 -0500

    YOUR A SPARK PLUG

--- Chapter4Practice/Quiz/index.js
+++ Chapter4Practice/Quiz/index.js
@@ -26,9 +26,46 @@ document.getElementById("carPartQuiz").addEventListener("submit",
         bedtimeResult = document.getElementById("bedtime").value;
         bedtimeHours = (Number)(bedtimeResult.split(":")[0]);
         bedtimeMinutes = (Number)(bedtimeResult.split(":")[1]);
+        if(bedtimeHours <= 3){
+            rim++;
+        }
+        if(bedtimeHours <= 9){
+            rearAxle++;
+        }
+        if(bedtimeHours <= 15){
+            sparkPlug++;
+        }
+        if(bedtimeHours <= 19){
+            exhaustPipe++;
+            sparkPlug++;
+        }
 
         gameQuizResult = document.getElementById("gameQuiz").value;
+        if(gameQuizResult == "Spotify"){
+            rim++;
+            frontRightFender++;
+        }
+        if(gameQuizResult == "Minecraft"){
+            rearAxle++;
+        }
+        if(gameQuizResult == "The Weather App"){
+            exhaustPipe++;
+        }
+        if(gameQuizResult == "Batman's Guide to Being Cool"){
+            sparkPlug++;
+            frontRightFender++;
+        }
+        if(gameQuizResult == "Grown Ups 2"){
+            sparkPlug++;
+            exhaustPipe++;
+        }
 
+        if(document.getElementById("marzahl").checked){
 
+        }
+
+        if(document.getElementById("pineapple").checked){
+            
+        }
     }
 )
