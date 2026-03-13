421fe780a6c852fc96a1830df064c2ad9a64f4cf
coolguy1333 <finglaws@stu.masd.k12.wi.us>
2026-03-13 14:03:17 -0500

    MINECRAFT

--- Chapter4Practice/Quiz/index.js
+++ Chapter4Practice/Quiz/index.js
@@ -1,14 +1,15 @@
 document.getElementById("carPartQuiz").addEventListener("submit",
     (event) => {
         event.preventDefault();
+       
         let rearAxle = 0;
         let frontRightFender = 0;
         let rim = 0;
         let exhaustPipe = 0;
         let sparkPlug = 0;
 
-        stateOfMatterResult = document.getElementById("stateOfMatter").ariaValueMax
-        if(stateOfMatterResult == "solid"){
+        stateOfMatterResult = document.getElementById('stateOfMatter').ariaValueMax;
+        if(stateOfMatterResult == "Solid"){
             rim++;
             frontRightFender++;
         }
@@ -21,22 +22,9 @@ document.getElementById("carPartQuiz").addEventListener("submit",
         if(stateOfMatterResult == "Whatever a NeeDoh is"){
             sparkPlug++;
         }
+       
+        bedtimeResult = document.getElementById("bedtime").value;
 
-
-
-        bedtimeResult = document.getElementById("bedtime").ariaValueMax
-        if(stateOfMatterResult == "solid"){
-            rim++;
-            frontRightFender++;
-        }
-        if(stateOfMatterResult == "Liquid"){
-            rearAxle++;
-        }
-        if(stateOfMatterResult == "Gas"){
-            exhaustPipe++;
-        }
-        if(stateOfMatterResult == "Whatever a NeeDoh is"){
-            sparkPlug++;
-        }
+        gameQuizResult = document.getElementById("gameQuiz").value;
     }
 )
