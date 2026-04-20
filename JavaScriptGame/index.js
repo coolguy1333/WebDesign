11346298f81f89f075a39d3beced4e70208eb1a7
coolguy1333 <finglaws@stu.masd.k12.wi.us>
2026-04-20 11:56:35 -0500

    fib click

--- JavaScriptGame/index.js
+++ JavaScriptGame/index.js
@@ -1,14 +1,57 @@
 let scraps = 0;
+let player = {
+    "currencies": {
+        "scrap": 1,
+        "armor": 0,
+    },
+    "scraps": {
+        "perClick": 1,
+        "prevClick": 0,
+        "fibClick": false,
+    },
+};
 
-document.getElementById("scrapsClicker").addEventListener(
-    "click", () => {
-        scraps++;
-        document.getElementById("scrapsOwned").textContent = scraps;
-    }
-)
+let storedData = localStorage.getItem("playerData")
+if(storedData){
+    player = JSON.parse(storedData)
+    document.getElementById("scrapsOwned").textContent = player.currencies.scrap;
+};
 
 document.getElementById("shopButton").addEventListener(
     "click", () => {
         window.location.href = "shop.html";
     }
+);
+
+document.getElementById("scrapsClicker").addEventListener(
+    "click", () => {
+        if(player.scraps.fibClick){
+            fibClick()
+        }
+        else{
+            normalClick()
+        }
+
+        saveGame()
+    }
 )
+
+function fibClick(){
+    player.currencies.scrap += player.scraps.perClick;
+
+    let nextFib = player.scraps.perClick + player.scraps.prevClick;
+
+    player.scraps.prevClick = player.scraps.perClick;
+    player.scraps.perClick = nextFib;
+
+    document.getElementById("scrapsOwned").textContent = player.currencies.scrap;
+}
+
+function normalClick(){
+    player.currencies.scrap += player.scraps.perClick
+    document.getElementById("scrapsOwned").textContent = player.currencies.scrap;
+}
+
+function saveGame(){
+    localStorage.setItem('playerData', JSON.stringify(player))
+}
--- JavaScriptGame/shop.js
+++ JavaScriptGame/shop.js
@@ -1,5 +1,39 @@
+let player = {
+    "currencies": {
+        "scrap": 1,
+        "armor": 0,
+    },
+    "scraps": {
+        "perClick": 1,
+        "prevClick": 0,
+        "fibClick": false,
+    },
+};
+
+let storedData = localStorage.getItem("playerData")
+if(storedData){
+    player = JSON.parse(storedData)
+    document.getElementById("testScraps").textContent = player.currencies.scrap;
+};
+
 document.getElementById("gameButton").addEventListener(
     "click", () => {
         window.location.href = "index.html";
     }
+);
+
+document.getElementById("scrapClickUpgrade").addEventListener(
+    "click", () => {
+        upgrade = document.getElementById("scrapClickUpgrade")
+        if(player.currencies.scrap >= upgrade.value){
+            player.currencies.scrap -= upgrade.value
+            player.scraps.perClick += 1
+            upgrade.value = math.floor(upgrade.value * 1.5)
+        }
+        saveGame()
+    }
 )
+
+function saveGame(){
+    localStorage.setItem('playerData', JSON.stringify(player))
+}
